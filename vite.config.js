20820eff021ae378ecbacd5382e244a9b2a5db7f
Lightlana888 <svetlana.yakuta@gmail.com>
2024-06-09 14:03:45 +0200

    adding baseUrl, building the project

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   base: "./",
   server: {
     proxy: {
-      // Проксирование всех запросов, начинающихся с '/api':
       "/api": {
-        target: "http://itgirlschool.justmakeit.ru",
+        target: "https://itgirlschool.justmakeit.ru",
         changeOrigin: true,
+        secure: true
       },
     },
   },
-
-})
+});
